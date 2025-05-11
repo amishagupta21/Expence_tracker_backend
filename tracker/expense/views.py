e8ccec6417e99c03316ade6a5fcafc43b76e975d
amishagupta21 <amisha@pcds.co.in>
2025-05-11 22:11:02 +0530

    added the model logic in the view for the current balance

--- tracker/expense/views.py
+++ tracker/expense/views.py
@@ -1,6 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
+from expense.models import TrackingHistory,CurrentBalance
 
-# Create your views here.
 
 def index(request):
+    if request.method=='POST':
+       amount=request.POST.get('amount')
+       description=request.POST.get('description')
+       current_balance,_ = CurrentBalance.objects.get_or_create(id=1)
+       expence_type = 'CREDIT'
+       if float(amount) < 0:
+           expence_type ='DEBIT'
+        
+       tracking_history=TrackingHistory.objects.create(
+          amount=amount,
+          current_balance=current_balance,
+          description=description,
+          expence_type = expence_type,
+         
+       )
+       current_balance.current_balance += float(tracking_history.amount)
+       current_balance.save()
+       return redirect('/')
     return render(request,'index.html')
