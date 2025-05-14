3984c3c64aab1c0a5b2930945451891e921be9de
amishagupta21 <amisha@pcds.co.in>
2025-05-14 23:51:33 +0530

    added the django-admin code

--- tracker/expense/admin.py
+++ tracker/expense/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from expense.models import *
 
-# Register your models here.
+
+admin.site.site_header="Expence tracker"
+admin.site.site_title="Expence tracker"
+admin.site.site_url="Expence tracker"
+admin.site.register(CurrentBalance)
+
+class TrackingHistoryAdmin(admin.ModelAdmin):
+       list_display=[
+                "amount",
+                "current_balance",
+                "description",
+                "expence_type",
+                "created_at"
+       ]
+       search_fields=[
+           "expence_type",
+           "description"
+       ]
+
+admin.site.register(TrackingHistory,TrackingHistoryAdmin)
--- tracker/expense/models.py
+++ tracker/expense/models.py
@@ -4,11 +4,18 @@ from django.db import models
 class CurrentBalance(models.Model):
       current_balance = models.FloatField(default=0)
       
+      def __str__(self):
+           return f"{self.current_balance}"
+      
       
 class TrackingHistory(models.Model):
-      current_balance=models.ForeignKey(CurrentBalance,on_delete=models.CASCADE)
-      amount=models.FloatField()
-      description= models.CharField(max_length=200)
-      expence_type = models.CharField(choices=(('CREDIT','CREDIT'),('DEBIT','DEBIT')),max_length=200)
-      updated_at = models.DateTimeField(auto_now=True)
-      created_at = models.DateTimeField(auto_now_add=True)
+    current_balance = models.ForeignKey(CurrentBalance, on_delete=models.CASCADE)
+    amount = models.FloatField()
+    description = models.CharField(max_length=200)
+    expence_type = models.CharField(choices=(('CREDIT', 'CREDIT'), ('DEBIT', 'DEBIT')), max_length=200)
+    updated_at = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.expence_type}: {self.description} - ${self.amount}"
+
